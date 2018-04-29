8bd0cc87add9b25341d81e04976b9b1ba5c097a7
Patty Kovash <pattykovash@gmail.com>
2018-04-29 13:14:34 -0600

    Add create route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal:true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
-  get '/about',  to: 'pages#about'
+  get '/about', to: 'pages#about'
+
+  resources :articles
 end
