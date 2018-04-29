483e29811ff361cc621a610b22e48689481c3940
Patty Kovash <pattykovash@gmail.com>
2018-04-29 17:34:54 -0600

    Create update action and view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,14 +9,41 @@ class ArticlesController < ApplicationController
 
   # Add new article
   def create
-    # render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = 'Article was successfully created!'
+      redirect_to article_path(@article)
+    else
+      flash[:notice] = 'We could not save your article.'
+      render :new
+    end
+  end
+
+  # Edit article
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  # Update article
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = 'Article was successfully updated!'
+      redirect_to article_path(@article)
+    else
+      flash[:notice] = 'We could not update your article.'
+      render :edit
+    end
+  end
+
+  # Show articles
+  def show
+    @article = Article.find(params[:id])
   end
 
   private
+
   def article_params
-    params.require(:article).permit(:title, :desciption)
+    params.require(:article).permit(:title, :description)
   end
 end
