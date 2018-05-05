5b9fb21d56298a7e4f6fe114b5c9376606cdac19
Patty Kovash <pattykovash@gmail.com>
2018-05-04 20:04:40 -0600

    Create destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -46,6 +46,14 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  # Delete articles
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = 'Article was successfully deleted.'
+    redirect_to articles_path
+  end
+
   private
 
   def article_params
